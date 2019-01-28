c53aefa265c17c8dfb8107d25cf7e7ec0b4ec1a6
heyjulieh <heyjulieh@gmail.com>
2019-01-27 21:54:06 -0800

    scroll snap working but nav scroll to section needs debugging

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // JavaScript source code
+var middle;
+var view_middle;
+
 $(document).ready(function () {
-    var navlink;
-    $('nav li').click(function (e) {
-        e.preventDefault();
-        navlink = $(this).attr('id');
-       $('html, body').animate({ scrollTop: $('div#'+navlink).offset().top }, 500, 'linear');
-    })
- 
+  var navlink;
+  var top;
+  $('nav li').click(function (e) {
+    e.preventDefault();
+    navlink = $(this).attr('id');
+    console.log('navlink is: ', navlink);
+    $('div#' + navlink).animate({ scrollTop: top }, 200);
+  })
+  $('nav h5').click(function (e) {
+    e.preventDefault();
+    $('nav div').removeClass('circle').addClass('full');
+    $('nav h5').addClass('hidden');
+    $('nav ul').removeClass('hidden');
+  })
+  $('.fa-arrow-up').click(function (e) {
+    e.preventDefault();
+    $('nav div').removeClass('full').addClass('circle');
+    $('nav ul').addClass('hidden');
+    $('nav h5').removeClass('hidden');
+  })
 });
