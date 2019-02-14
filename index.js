4a6d1b9864d0321e7d3877f839c921fe61e06896
heyjulieh <heyjulieh@gmail.com>
2019-02-14 14:06:07 -0800

    Added modal logic.

--- index.js
+++ index.js
@@ -5,19 +5,41 @@ $(document).ready(function () {
         e.preventDefault();
         navlink = $(this).attr('id');
        $('html, body').animate({ scrollTop: $('div#'+navlink).offset().top }, 500, 'linear');
-    })
+    });
     $('nav h5').click(function (e) {
         e.preventDefault();
         $('nav div').removeClass('circle').addClass('full');
         $('nav h5').addClass('hidden');
         $('nav ul').removeClass('hidden');
-    })
+    });
     $('.fa-arrow-up').click(function (e) {
         e.preventDefault();
         $('nav div').removeClass('full').addClass('circle');
         $('nav ul').addClass('hidden');
         $('nav h5').removeClass('hidden');
-    })
+    });
+    $('#services li').click(function(e) {
+        e.preventDefault();
+        elem = $(this);
+        button = $(this).attr('id')
+        if ($('section.' + button).hasClass('hidden')) {
+            $('section.' + button).removeClass('hidden');
+            $('.modal').not('section.' + button).addClass('hidden');
+        } else {
+            $('.modal').addClass('hidden');
+        }
+    });
+    $('section .close').click(function(e) {
+        e.preventDefault();
+        if ($('section.' + button).hasClass('hidden')==false) { 
+            $('section.' + button).addClass('hidden')
+        }
+    });
+   $(document).click(function(event) {
+      if (!$(event.target).closest(".modal, #services li").length && $('section.' + button).hasClass('hidden')==false) {
+        $('body').find('section.' + button).addClass('hidden');
+      }
+    });
     var intro_top = $('div#intro').offset().top;
     var services_top = $('div#services').offset().top;
     var about_top = $('div#about').offset().top;
